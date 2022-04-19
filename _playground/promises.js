dc83dca2c33f0beafa964ef0081e5bab2fb42879
Joseph Villajin <resetdesigns@gmail.com>
2022-04-19 11:53:01 -0700

    Working With Promises

---
+++ _playground/promises.js
@@ -0,0 +1,49 @@
+// Callback Refresher
+// const doWorkCallback = (callback) => {
+//     setTimeout(() => {
+//         // Example error: callback('This is my error!', undefined);
+//         // Example Result
+//         callback(undefined, [1, 2, 3, 4]);
+//     }, 2000);
+// };
+
+// doWorkCallback((error, result) => {
+//     // do something once we get the error or result comes back from original function
+//     // needs conditional logic -- runs for failures and successes
+//     if (error) {
+//         // use return to stop function execution
+//         return console.log(error);
+//     }
+
+//     console.log(result);
+// });
+
+// Promises
+const doWorkPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        // instead of passing arg to the callback function to denote how the requestor should handle we have two functions
+        // success
+        // resolve([1, 2, 3, 4]);
+        // failure
+        reject('Things went wrong');
+        // cannot be called twice
+        reject('New error');
+        resolve([1, 2, 3, 4]);
+    }, 2000);
+});
+
+doWorkPromise
+    .then((result) => {
+        console.log('Success', result);
+    })
+    .catch((error) => {
+        console.log('Error', error);
+    });
+
+//
+//								fulfilled
+//								/
+//	Promise		-- pending -->
+//								\
+//								rejected
+//
