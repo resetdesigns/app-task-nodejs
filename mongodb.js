388b895da782785a80676d53833fa66fe2d87610
Joseph Villajin <resetdesigns@gmail.com>
2022-04-19 11:25:53 -0700

    Mongo DB Connection

--- mongodb.js
+++ mongodb.js
@@ -1,6 +1,10 @@
 // CRUD - Create, Read, Update, Delete
-const mongodb = require('mongodb');
-const MongoClient = mongodb.MongoClient;
+// const mongodb = require('mongodb');
+// const MongoClient = mongodb.MongoClient;
+// const ObjectID = mongodb.ObjectID;
+
+// Destructured Shorthand
+const { MongoClient, ObjectID } = require('mongodb');
 
 const connectionURL = 'mongodb://127.0.0.1:27017';
 const databaseName = 'task-manager';
@@ -17,18 +21,40 @@ MongoClient.connect(
 
         const db = client.db(databaseName);
 
-        db.collection('users').insertOne(
-            {
-                name: 'Joseph',
-                age: 33,
-            },
-            (error, result) => {
-                if (error) {
-                    return console.log('Unable to insert user');
-                }
+        // db.collection('users').findOne({ name: 'Kim' }, (error, user) => {
+        //     if (error) {
+        //         return console.log('Unable to fetch user');
+        //     }
 
-                console.log(result.ops);
+        //     console.log(user);
+        // });
+
+        // db.collection('users')
+        //     .find({ age: 33 })
+        //     .toArray((error, users) => {
+        //         console.log(users);
+        //     });
+
+        // db.collection('users')
+        //     .find({ age: 33 })
+        //     .count((error, count) => {
+        //         console.log(count);
+        //     });
+
+        db.collection('tasks').findOne({ _id: new ObjectID('625ef1fac192ce2ae0de7530') }, (error, task) => {
+            if (error) {
+                return console.log('Unable to fetch task!');
             }
-        );
+            console.log(task);
+        });
+
+        db.collection('tasks')
+            .find({ completed: false })
+            .toArray((error, tasks) => {
+                if (error) {
+                    return console.log('Unable to find task');
+                }
+                console.log(tasks);
+            });
     }
 );
