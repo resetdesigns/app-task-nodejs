1b8508ead87807c42066c700bdf4fbe42dae86f4
Joseph Villajin <resetdesigns@gmail.com>
2022-04-20 15:00:16 -0700

    refactored api endpoints with async await

--- _playground/mongoose-promise-chaining-2.js
+++ _playground/mongoose-promise-chaining-2.js
@@ -1,13 +1,29 @@
 require('../src/db/mongoose');
 const Task = require('../src/models/task');
 
-Task.findByIdAndDelete('625f3c456e3b14bc534837b9')
-    .then(() => {
-        console.log('Task removed');
-        return Task.countDocuments({ completed: false });
-    })
-    .then((result) => {
-        console.log(result);
+// Promise Chaining
+// Task.findByIdAndDelete('625f3c456e3b14bc534837b9')
+//     .then(() => {
+//         console.log('Task removed');
+//         return Task.countDocuments({ completed: false });
+//     })
+//     .then((result) => {
+//         console.log(result);
+//     })
+//     .catch((error) => {
+//         console.log(error);
+//     });
+
+// Async Await
+const deleteTaskAndCount = async (id) => {
+    const task = await Task.findByIdAndDelete(id);
+    const count = await Task.countDocuments({ completed: false });
+    return count;
+};
+
+deleteTaskAndCount('6260311ca47a297d8415fbfc')
+    .then((count) => {
+        console.log(count);
     })
     .catch((error) => {
         console.log(error);
