2fc42125973da1cb55241471445d694fad4a6992
Joseph Villajin <resetdesigns@gmail.com>
2022-04-21 15:07:48 -0700

    Added Auth & Login Endpoint

--- src/models/user.js
+++ src/models/user.js
@@ -10,6 +10,7 @@ const userSchema = new mongoose.Schema({
     },
     email: {
         type: String,
+        unique: true,
         required: true,
         trim: true,
         lowercase: true,
@@ -41,7 +42,21 @@ const userSchema = new mongoose.Schema({
     },
 });
 
-// userSchema.statics.findByCredentials;
+userSchema.statics.findByCredentials = async (email, password) => {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+        throw new Error('Unable to login');
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+        throw new Error('Unable to login');
+    }
+
+    return user;
+};
 
 // Hash the plain text password before saving
 userSchema.pre('save', async function (next) {
